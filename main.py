5b7fc3959937b9a594191a645cb9e6aed31bd501
William Ma <williamm360277@gmail.com>
2026-01-07 16:44:25 -0500

    venv

---
+++ main.py
@@ -0,0 +1,29 @@
+import matplotlib.plt as pyplot
+import os
+
+def load_map(map_name):
+    try:
+        with open("",r) as r:
+            ...
+
+    except:
+        ...
+
+
+
+def read_data():
+    ...
+
+
+def pearson():
+    ...
+
+
+def main():
+    ...
+
+def file_path():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return current_dir
+
+file_path()
