b6ed4380a9b94a589fd84429da8332ffbd1f6001
William Ma <williamm360277@gmail.com>
2026-01-07 17:11:15 -0500

    nav

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import matplotlib.plt as pyplot
+# import matplotlib.plt as pyplot
 import os
 
-def load_map(map_name):
+
+def load_map(map_name: str):
     try:
-        with open("",r) as r:
-            ...
+        parent_dir = dir_path() + "/navigation_data/maps/" + map_name
+        map_location_png = parent_dir + "/map.png"
+        map_location_yaml = parent_dir + "/map.yaml"
+        print(map_location_yaml)
+        with open(map_location_yaml, "r") as file:
+            for line in file:
+                print(line)
 
     except:
-        ...
-
+        print("error")
 
 
 def read_data():
@@ -22,8 +27,13 @@ def pearson():
 def main():
     ...
 
-def file_path():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    return current_dir
 
-file_path()
+def dir_path():
+    main_dir = os.path.dirname(os.path.abspath(
+        __file__)).removesuffix("\main.py")
+    return main_dir
+
+
+print(dir_path())
+
+load_map("map1")
